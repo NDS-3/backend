056d76417036b0dd5c4b9fa48874c33a9ab3533f
hyejin <hy2jinl22@gmail.com>
2022-11-02 16:51:55 +0900

    chore: 디버깅

--- app/router/letters.py
+++ app/router/letters.py
@@ -57,7 +57,7 @@ async def get_letter_detail_by_jwt(letter_id: int, auth: CognitoToken = Depends(
     db_owner = await OwnerRepository.fetch_by_id(db_letter.owner_id)
     if db_owner.cognito_id != auth.username:
         raise HTTPException(status_code=401, detail='편지 주인만 편지를 확인할 수 있습니다.')
-
+    print(db_owner.cognito_id, auth.username)
     sticker = await StickerRepository.fetch_by_id(db, _id=db_letter.sticker_id)
 
     response_letter = LetterDetail(id=db_letter.id, sticker=sticker, content=db_letter.content)
